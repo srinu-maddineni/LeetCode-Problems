ab4e44554d901fd6707105009b669a203b6e2a84
srinu-maddineni <maddineis76@gmail.com>
2025-09-23 15:54:37 +0000

    [LeetCode Sync] Runtime - 15 ms (5.52%), Memory - 13.3 MB (12.21%)

---
+++ leetcode/0617-merge-two-binary-trees/solution.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def mergeTrees(self, root1, root2):
+        if not root1: return root2
+        if not root2: return root1
+        root = TreeNode(root1.val+root2.val)
+        root.left= self.mergeTrees(root1.left,root2.left)
+        root.right = self.mergeTrees(root1.right,root2.right)
+        return root
+
+
+
+        print(root3.val)
+
+        """
+        :type root1: Optional[TreeNode]
+        :type root2: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
+        """
+        
