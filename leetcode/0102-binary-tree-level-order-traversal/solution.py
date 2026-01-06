02e976dcdd07fc630bdafc3edabaf64fc29a4a8f
srinu-maddineni <maddineis76@gmail.com>
2026-01-06 05:23:38 +0000

    [LeetCode Sync] Runtime - 4 ms (5.89%), Memory - 20.1 MB (24.64%)

--- leetcode/0102-binary-tree-level-order-traversal/solution.py
+++ leetcode/0102-binary-tree-level-order-traversal/solution.py
@@ -1,37 +1,26 @@
-from collections import deque
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def levelOrder(self, root):
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-        q = deque()
-        q.append(root)
-        arr = []
-        i = 0
-        k = i
-        while q:
-            l = len(q)
-            s= []
-            for i in range(l):
-                node = q.popleft()
-                s.append(node.val)
-                if node.left: q.append(node.left)
-                if node.right: q.append(node.right)
 
-            
-            arr.append(s[:])
-
-
-
-        return arr
-
-        """
-        :type root: Optional[TreeNode]
-        :rtype: List[List[int]]
-        """
+        quee = deque([root])
+        r = []
+        while quee:
+            length_level = len(quee)
+            sol = []
+            for i in range(length_level):
+                node = quee.popleft()
+                sol.append(node.val)
+                if node.left:
+                    quee.append(node.left)
+                if node.right:
+                    quee.append(node.right)
+            r.append(sol[:])
+        return r
         
