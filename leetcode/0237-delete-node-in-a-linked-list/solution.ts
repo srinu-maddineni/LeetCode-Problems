a954bdccfbc7717f4fff6987f1ae43c10ef6fe2f
srinu-maddineni <maddineis76@gmail.com>
2026-03-18 06:20:55 +0000

    [LeetCode Sync] Runtime - 54 ms (44.90%), Memory - 58.6 MB (32.65%)

---
+++ leetcode/0237-delete-node-in-a-linked-list/solution.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify it in-place instead.
+ */
+function deleteNode(node: ListNode | null): void {
+    node.val = node.next.val
+    node.next=node.next.next
+    // while(curr.next){
+    //     if(curr.next.val === node.val){
+    //         curr.next = curr.next.next
+    //     }
+    // }
+};
