cf320506abfff380f851d769bb770150dc04c211
srinu-maddineni <maddineis76@gmail.com>
2026-05-04 09:54:26 +0000

    [LeetCode Sync] Runtime - 6 ms (5.24%), Memory - 58.2 MB (7.86%)

---
+++ leetcode/0048-rotate-image/solution.ts
@@ -0,0 +1,21 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix: number[][]): void {
+     let n= matrix.length
+    
+    for(let i=0;i<n;i++){
+        for(let j=i;j<n;j++){
+            let temp = matrix[i][j]
+             matrix[i][j] = matrix[j][i]
+             matrix[j][i] =temp
+
+        }
+    }
+    for(let i=0;i<n;i++){
+        matrix[i].reverse()
+    }
+    // matrix =m
+    console.log(matrix)
+    
+};
