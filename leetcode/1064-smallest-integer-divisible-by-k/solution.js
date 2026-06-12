94af8cb1f92f4dd7d29f2b997f87ade41784e4dc
srinu-maddineni <maddineis76@gmail.com>
2026-06-12 16:54:57 +0000

    [LeetCode Sync] Runtime - 3 ms (51.43%), Memory - 53.6 MB (92.86%)

---
+++ leetcode/1064-smallest-integer-divisible-by-k/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} k
+ * @return {number}
+ */
+var smallestRepunitDivByK = function(k) {
+  if(k%2==0 || k%5==0){
+    return -1
+  }  
+
+  let rem = 0
+  for(let i=0;i<k;i++){
+    rem = (rem*10+1)%k
+    if(rem===0){
+        return i+1
+    }
+  }
+  return -1
+};
