40deff3ba634065c89e3148efdd8caa3c3bb0efe
srinu-maddineni <maddineis76@gmail.com>
2026-06-21 15:52:06 +0000

    [LeetCode Sync] Runtime - 1 ms (94.41%), Memory - 65 MB (74.28%)

--- leetcode/0121-best-time-to-buy-and-sell-stock/solution.js
+++ leetcode/0121-best-time-to-buy-and-sell-stock/solution.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let profit = 0
-    let minPrice = Infinity
-    for(let price of prices){
-         minPrice = Math.min(price,minPrice)
-        let p =price- minPrice
-        profit = Math.max(p,profit)
+    let prof = 0
+    let currMin = prices[0]
+    for(let i=0;i<prices.length;i++){
+        let curr = prices[i] - currMin
+        prof  = Math.max(prof,curr)
+        currMin = Math.min(currMin,prices[i])
     }
-    return profit
+    return prof
 };
