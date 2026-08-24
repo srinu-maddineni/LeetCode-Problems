1cfd44f372e65fe1f548bf0ddcb39be3fd99690e
srinu-maddineni <147825460+srinu-maddineni@users.noreply.github.com>
2026-08-24 21:04:47 +0530

    Time: 1011 ms (7.69%) | Memory: 19.6 MB (78.46%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
@@ -9,25 +9,37 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+        # curr = head
+        # res = ''
+        
+        # while curr:
+        #     res+=str(curr.val)
+        #     curr = curr.next
+        # print(res)
+        # res = str(int(res)*2)
+        # print(res)
+        # dummy = ListNode(0)
+        # curr = dummy
+        # i=0
+        # while i<len(res):
+        #     curr.next = ListNode(int(res[i]))
+        #     i+=1
+        #     curr = curr.next
+        # return dummy.next
         curr = head
-        res = ''
+        if head.val >=5:
+            head = ListNode(1,head)
         
         while curr:
-            res+=str(curr.val)
+        
+            curr.val = (curr.val*2)%10
+            print(curr.val)
+            if curr.next and curr.next.val>=5:
+                curr.val = curr.val+1
             curr = curr.next
-        print(res)
-        res = str(int(res)*2)
-        print(res)
-        dummy = ListNode(0)
-        curr = dummy
-        i=0
-        while i<len(res):
-            curr.next = ListNode(int(res[i]))
-            i+=1
-            curr = curr.next
-        return dummy.next
+        return head
 
-        print(res)
+        
 
 
         
