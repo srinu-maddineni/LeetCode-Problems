66177f6808eb2fcf13e0b334de2dc306de11600d
srinu-maddineni <147825460+srinu-maddineni@users.noreply.github.com>
2026-08-25 20:38:45 +0530

    Time: 7 ms (9.20%) | Memory: 12.3 MB (50.57%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
@@ -5,10 +5,21 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        s = set(nums)
-        print(s)
-        i = 1
-        while True:
-            if i*k not in s:
-                return i*k
-            i+=1
+        # s = set(nums)
+        # print(s)
+        # i = 1
+        # while True:
+        #     if i*k not in s:
+        #         return i*k
+        #     i+=1
+        nums.sort()
+        j =1
+        for i in range(len(nums)):
+            if j*k > nums[i]:
+                continue
+            elif j*k == nums[i]:
+                j+=1
+                continue
+            else: return j*k
+            j+=1
+        return (j)*k
