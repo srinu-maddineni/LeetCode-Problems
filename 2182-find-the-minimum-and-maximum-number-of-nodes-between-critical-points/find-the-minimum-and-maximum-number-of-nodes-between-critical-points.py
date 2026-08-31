20cfb5c27554313fca4a35eee2dd7f0f0e2c60ce
srinu-maddineni <147825460+srinu-maddineni@users.noreply.github.com>
2026-08-31 22:13:53 +0530

    Time: 202 ms (83.64%) | Memory: 90.6 MB (65.45%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -15,19 +15,34 @@ class Solution(object):
         prev = head
         curr = head.next
         i=2
+        mi = float('inf')
+        last =0
+        first =0
         while curr.next:
             if (curr.val>prev.val and curr.val>curr.next.val) or (curr.val<prev.val and curr.val<curr.next.val):
-                critical.append(i)
+                if first== 0:
+                    first = i
+                if last !=0:
+                    mi = min(mi,i-last)
+                    last = i
+                else:
+                    last = i
+                 
+                # critical.append(i)
             i+=1
             curr=curr.next
             prev=prev.next
-        if len(critical)<2:
-            return [-1,-1]
-        mi = float('inf')
-        for i in range(1,len(critical)):
-            mi = min(mi,critical[i]-critical[i-1])
+        # if len(critical)<2:
+        #     return [-1,-1]
+        # mi = float('inf')
+        # for i in range(1,len(critical)):
+        #     mi = min(mi,critical[i]-critical[i-1])
+        # print(mi)
+        # return [mi,critical[-1]-critical[0]]
         print(mi)
-        return [mi,critical[-1]-critical[0]]
+        if mi == float('inf'):
+            return [-1,-1]
+        return [mi,last-first]
 
 
         
