c71538be3dc16a2bbdf62e1a800036e207e8c6e1
srinu-maddineni <147825460+srinu-maddineni@users.noreply.github.com>
2026-09-03 20:18:21 +0530

    Time: 99 ms (25.00%) | Memory: 35.1 MB (93.90%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+
+        small = False
+        big = False
+        mn = float('inf')
+        for i in range(len(nums1)):
+            if nums1[i] %2 !=0:
+                small = True
+            else:
+                big = True
+            mn = min(mn,nums1[i])
+
+        if not small  or not big:
+            return True
+        
+        return min(nums1) % 2 !=0
+
+        
