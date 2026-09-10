c0fecf237439e4f963266bb57bc8fbf75ec00d6c
srinu-maddineni <147825460+srinu-maddineni@users.noreply.github.com>
2026-09-10 23:41:32 +0530

    Time: 54 ms (42.46%) | Memory: 19.6 MB (33.89%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.py
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.py
@@ -6,27 +6,31 @@
 #         self.right = right
 class Solution:
     def averageOfSubtree(self, root: TreeNode) -> int:
-
+        ans = 0
         def post(root):
+            nonlocal ans
             if not root:
                 return 0,0
             l,cl =post(root.left)
             r,cr = post(root.right)
             s = root.val+l+r
             c = 1+cl+cr
-            return s,c
-        print(post(root))
-
-        queue = [root]
-        ans =0
-        while queue:
-            node = queue.pop()
-            k ,n= post(node)
-            if (k//n) == node.val:
+            if s//c ==root.val:
                 ans+=1
-            if node.left:queue.append(node.left)
-            if node.right:queue.append(node.right)
 
+            return s,c
+        # print(post(root))
+
+        # queue = [root]
+        # ans =0
+        # while queue:
+        #     node = queue.pop()
+        #     k ,n= post(node)
+        #     if (k//n) == node.val:
+        #         ans+=1
+        #     if node.left:queue.append(node.left)
+        #     if node.right:queue.append(node.right)
+        post(root)
         return ans
 
 
