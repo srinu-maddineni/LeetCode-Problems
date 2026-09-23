ce298e5d5046cdf42a9f28dc7c9295f2784f53f2
srinu-maddineni <147825460+srinu-maddineni@users.noreply.github.com>
2026-09-23 19:53:57 +0530

    Time: 60 ms (88.85%) | Memory: 31.1 MB (41.90%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
@@ -1,32 +1,49 @@
 class Solution:
     def minOperations(self, nums: list[int], x: int) -> int:
-        op = float('inf')
+        # op = float('inf')
+        # n = len(nums)
+        # if nums[0] ==x or nums[n-1] ==x:
+        #     return 1
+        
+        # prev =[nums[0]]
+        # suff={}
+        
+        # s =nums[n-1]
+        # suff[s] = 1
+        # for i in range(1,n):
+        #     s1=prev[i-1]+nums[i]
+        #     if s1==x:
+        #         op = min(op,i+1)
+        #     prev.append(s1)
+        #     s+=nums[n-i-1]
+
+        #     if s ==x:
+        #         op = min(op,i+1)
+        #     suff[s] = i+1
+        # if prev[n-1] <x:
+        #     return -1
+        # for i in range(n):
+        #     t = x-prev[i]
+        #     if suff.get(t):
+        #         op = min(op,i+1+suff[t])
+        # if op ==float('inf'):return -1
+        # return op
+
         n = len(nums)
-        if nums[0] ==x or nums[n-1] ==x:
-            return 1
+        t = sum(nums)-x
+        if t<0: return -1
+        if t==0:return n 
+        print(t)
+        l = 0
         
-        prev =[nums[0]]
-        suff={}
-        
-        s =nums[n-1]
-        suff[s] = 1
-        for i in range(1,n):
-            s1=prev[i-1]+nums[i]
-            if s1==x:
-                op = min(op,i+1)
-            prev.append(s1)
-            s+=nums[n-i-1]
-
-            if s ==x:
-                op = min(op,i+1)
-            suff[s] = i+1
-        if prev[n-1] <x:
-            return -1
+        j=0
+        s =0
         for i in range(n):
-            t = x-prev[i]
-            if suff.get(t):
-                op = min(op,i+1+suff[t])
-        if op ==float('inf'):return -1
-        return op
-
-       
+            s += nums[i]
+            while s>t:
+                s-=nums[j]
+                j+=1
+            if s ==t:
+                l=max(l,i-j+1)
+        if l==0:return -1
+        return n - l
